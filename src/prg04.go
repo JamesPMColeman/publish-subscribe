9bc550152f3f8fb2164174f23c0e803f3f9a62e7
James Coleman <jcolem22@msudenver.edu>
2020-11-30 22:59:43 -0700

    Simple code in main
    Added empty lists of strings that I'll fill with info
    to be subscribed to. Also added calls to publisher and
    subscriber. Nothing is connected yet, I'm still figuring
    out how channels and the PubSub is supposed to work.

--- src/prg04.go
+++ src/prg04.go
@@ -1,9 +1,20 @@
-/*
- * CS3210 - Principles of Programming Languages - Fall 2020
- * Instructor: Thyago Mota
- * Description: Prg04 - Publish Subscribe Simulation
- * Student(s) Name(s):
- */
+/************************************************************************
+*																		*
+*																		*
+*			James Coleman												*
+*			CS 3210														*
+*			Project 5													*
+*			November 30th												*
+*																		*
+************************************************************************/
+
+
+	   /*
+		*		Thanks to youthincmag.com for instrument facts
+		*
+		*
+		*
+		*/
 
 package main
 
@@ -21,8 +32,8 @@ var wg sync.WaitGroup
 // TODO: creates and returns a new channel on a given topic, updating the PubSub struct
 func (ps PubSub) subscribe(topic string) chan string {
 	ps.mu.Lock()
-	ps.topics[topic]
 	
+	ps.topics[topic] = append(ps.topics[topic], 
 	ps.mu.Unlock()
 	return nil
 }
@@ -39,7 +50,7 @@ func publisher(ps PubSub, topic string, msgs[]string) {
 
 // TODO: reads and displays all messages received from a particular topic
 func subscriber(ps PubSub, name string, topic string) {
-
+	factsChannel := ps.Subscribe(topic)
 }
 
 func main() {
@@ -48,15 +59,36 @@ func main() {
 	ps := PubSub{topics: make(map[string][]chan string)}
 
 	// TODO: create the arrays of messages to be sent on each topic
+	riverFacts := []string{
+		...
+	}
 
+	cityFacts := []string{
+		...
+	}
 
-	// TODO: set wait group to 2 (# of publishers)
+	desertFacts := []string{
+		...
+	}
 
-	// TODO: create the publisher goroutines
+	// Done: set wait group to 2 (# of publishers)
+	wg.Add(2)
 
+	// Done: create the publisher goroutines
+	go publisher(ps, "River Facts", riverFacts)
+	go publisher(ps, "City Facts", cityFacts)
+	go publisher(ps, "Desert Facts", desertFacts)
 
-	// TODO: create the subscriber goroutines
+	// Done: create the subscriber goroutines
+	go subscriber(ps, "Anna", riverFacts)
+	go subscriber(ps, "Anna", desertFacts)
+	go subscriber(ps, "Ben", riverFacts)
+	go subscriber(ps, "Ben", cityFacts)
+	go subscriber(ps, "Ben", desertFacts)
+	go subscriber(ps, "Cal", desertFacts)
+	go subscriber(ps, "Danny", riverFacts)
+	go subscriber(ps, "Danny", cityFacts)
 
-
-	// TODO: wait for all publishers to be done
+	// Done: wait for all publishers to be done
+	wg.Wait()
 }
