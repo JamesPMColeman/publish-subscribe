607c79b1a91b548caca497bd3a36b848eac02af1
James Coleman <jcolem22@msudenver.edu>
2020-12-02 12:30:00 -0700

    Bit of a mess right now. Still trying to figure out map syntax

--- src/prg04.go
+++ src/prg04.go
@@ -20,11 +20,14 @@ package main
 
 import (
 	"sync";
-	"fmt"
+	"fmt";
+	"time";
+	"math/rand";
 )
 
 type PubSub struct {
 	mu sync.Mutex
+	writeMu sync.RWMutex
 	topics map[string][]chan string
 }
 
@@ -33,26 +36,50 @@ var wg sync.WaitGroup
 // TODO: creates and returns a new channel on a given topic, updating the PubSub struct
 func (ps PubSub) subscribe(topic string) chan string {
 	ps.mu.Lock()
-	fmt.Println("subscribe")
-	ps.topics[topic] = append(ps.topics[topic], "Yo")
+	fmt.Println("Subscribe")
+	factChannel := make(chan string)
+	//fmt.Println(factChannel)
+	ps.topics[topic] = append(ps.topics[topic], factChannel)
 	ps.mu.Unlock()
-	return nil
+	return factChannel
 }
 
 // TODO: writes the given message on all the channels associated with the given topic
-func (ps PubSub) publish(topic string, facts string) {
+func (ps PubSub) publish(topic string, fact string) {
+	ps.writeMu.RLock()
+	fmt.Println(fact)
 	fmt.Println("Publish")
+	for {
+		ps.topics[topic]
+		go func(ps.topics[topic] <-fact)()
+	}
+	fmt.Println(len(ps.topics))
+	fmt.Println(len(ps.topics[topic]))
+	ps.writeMu.RUnlock()
 }
 
 // TODO: sends messages taken from a given array of message, one at a time and at random intervals, to all topic subscribers
 func publisher(ps PubSub, topic string, facts[]string) {
+	
 	fmt.Println("Publisher")
+	for i := range facts{
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+		ps.publish(topic, facts[i])
+	}
+	
 }
 
 // TODO: reads and displays all messages received from a particular topic
 func subscriber(ps PubSub, name string, topic string) {
-	factsChannel := ps.Subscribe(topic)
 	fmt.Println("Subscriber")
+	factsChannel := ps.subscribe(topic)
+	fmt.Println("Right after call to subscribe")
+	fmt.Println(factsChannel)
+	for {
+		fact := <- factsChannel 
+		fmt.Println(name + " received: " + fact)
+	}
+	fmt.Println("What keeps subscriber from terminating immediately")
 }
 
 func main() {
@@ -63,19 +90,19 @@ func main() {
 	// Done: create the arrays of messages to be sent on each topic
 	riverFacts := []string{
 		"The Colorado river has not reached the sea since 1998",
-		"Three Gorges Dam on the Yangtze river in china, holds so much waterthat it slowed the rotation of the Earth ever so slightly",
-		"The Bolton Stride in England is only about six feet accross but it is so deep, know one can say where the bottom is. Becuase of this fact the stride is very dangerous",
+		"Three Gorges Dam on the Yangtze river in china, holds so much water that it slowed the rotation of the Earth ever so slightly",
+		"The Bolton Stride in England is only about six feet across but it is so deep, know one can say where the bottom is. Because of this fact the stride is very dangerous",
 	}
 
 	cityFacts := []string{
 		"Paris has no stop signs",
-		"Seattle has more hoseholds with pets than with children",
+		"Seattle has more households with pets than with children",
 		"Mexico City is built on lake Texcoco, as a result it is constantly sinking",
 		"In Calcutta more people use bikes then cars to get around",
 	}
 
 	desertFacts := []string{
-		"Saying 'Sahara desert' is redundent. The word sahara means desert in Arabic",
+		"Saying 'Sahara desert' is redundant. The word sahara means desert in Arabic",
 		"The largest desert in the world is Antarctica",
 		"Sand dunes 'sing' as the shift",
 	}
@@ -89,14 +116,14 @@ func main() {
 	go publisher(ps, "Desert Facts", desertFacts)
 
 	// Done: create the subscriber goroutines
-	go subscriber(ps, "Anna", riverFacts)
-	go subscriber(ps, "Anna", desertFacts)
-	go subscriber(ps, "Ben", riverFacts)
-	go subscriber(ps, "Ben", cityFacts)
-	go subscriber(ps, "Ben", desertFacts)
-	go subscriber(ps, "Cal", desertFacts)
-	go subscriber(ps, "Danny", riverFacts)
-	go subscriber(ps, "Danny", cityFacts)
+	go subscriber(ps, "Anna", "riverFacts")
+	go subscriber(ps, "Anna", "desertFacts")
+	go subscriber(ps, "Ben", "riverFacts")
+	go subscriber(ps, "Ben", "cityFacts")
+	go subscriber(ps, "Ben", "desertFacts")
+	go subscriber(ps, "Cal", "desertFacts")
+	go subscriber(ps, "Danny", "riverFacts")
+	go subscriber(ps, "Danny", "cityFacts")
 
 	// Done: wait for all publishers to be done
 	wg.Wait()
