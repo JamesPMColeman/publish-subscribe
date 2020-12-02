2ead339c52783be5471b1566180ed89dfe12ac81
James Coleman <jcolem22@msudenver.edu>
2020-12-01 19:02:45 -0700

    Mostly finished main. Working on methods and functions

--- src/prg04.go
+++ src/prg04.go
@@ -19,7 +19,8 @@
 package main
 
 import (
-	"sync"
+	"sync";
+	"fmt"
 )
 
 type PubSub struct {
@@ -32,25 +33,26 @@ var wg sync.WaitGroup
 // TODO: creates and returns a new channel on a given topic, updating the PubSub struct
 func (ps PubSub) subscribe(topic string) chan string {
 	ps.mu.Lock()
-	
-	ps.topics[topic] = append(ps.topics[topic], 
+	fmt.Println("subscribe")
+	ps.topics[topic] = append(ps.topics[topic], "Yo")
 	ps.mu.Unlock()
 	return nil
 }
 
 // TODO: writes the given message on all the channels associated with the given topic
-func (ps PubSub) publish(topic string, msg string) {
-
+func (ps PubSub) publish(topic string, facts string) {
+	fmt.Println("Publish")
 }
 
 // TODO: sends messages taken from a given array of message, one at a time and at random intervals, to all topic subscribers
-func publisher(ps PubSub, topic string, msgs[]string) {
-
+func publisher(ps PubSub, topic string, facts[]string) {
+	fmt.Println("Publisher")
 }
 
 // TODO: reads and displays all messages received from a particular topic
 func subscriber(ps PubSub, name string, topic string) {
 	factsChannel := ps.Subscribe(topic)
+	fmt.Println("Subscriber")
 }
 
 func main() {
@@ -58,17 +60,24 @@ func main() {
 	// TODO: create the ps struct
 	ps := PubSub{topics: make(map[string][]chan string)}
 
-	// TODO: create the arrays of messages to be sent on each topic
+	// Done: create the arrays of messages to be sent on each topic
 	riverFacts := []string{
-		...
+		"The Colorado river has not reached the sea since 1998",
+		"Three Gorges Dam on the Yangtze river in china, holds so much waterthat it slowed the rotation of the Earth ever so slightly",
+		"The Bolton Stride in England is only about six feet accross but it is so deep, know one can say where the bottom is. Becuase of this fact the stride is very dangerous",
 	}
 
 	cityFacts := []string{
-		...
+		"Paris has no stop signs",
+		"Seattle has more hoseholds with pets than with children",
+		"Mexico City is built on lake Texcoco, as a result it is constantly sinking",
+		"In Calcutta more people use bikes then cars to get around",
 	}
 
 	desertFacts := []string{
-		...
+		"Saying 'Sahara desert' is redundent. The word sahara means desert in Arabic",
+		"The largest desert in the world is Antarctica",
+		"Sand dunes 'sing' as the shift",
 	}
 
 	// Done: set wait group to 2 (# of publishers)
